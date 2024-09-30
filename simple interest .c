d8af62f683efec90ade6c5f0da6042ffcf89676a
Jepkuruistella <jepkurui.22345@students.kyu.ac.ke>
2024-09-30 02:07:56 -0700

    Add files via upload

---
+++ simple interest .c
@@ -0,0 +1,18 @@
+//program to calculate simpleinterest
+/*
+program to calculate simpleinterest
+Author:stella
+Date:23/09/2024
+*/
+#include <stdio.h>
+int main(){
+int principal,time,rate, simpleinterest;
+  printf("enter principal:");
+  scanf("%d",&principal);
+  printf("enter rate:");
+  scanf("%d",&rate);
+  printf("enter time:");
+  simpleinterest=principal*rate*time/100;
+  printf("simpleinterest is%d\n, simpleinterest");
+  return 0;
+  }
