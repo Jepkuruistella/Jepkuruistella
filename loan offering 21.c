88c0f7579cfe095bda0e9970345108e5947d9331
Jepkuruistella <jepkurui.22345@students.kyu.ac.ke>
2024-10-02 07:39:13 -0700

    Add files via upload

---
+++ loan offering 21.c
@@ -0,0 +1,18 @@
+//program to offer a customer loan
+/*
+program to accept user prompting response to age and income
+Author: stella jepkurui
+Date: 29/09/2024
+*/
+#include<stdio.h>
+int main (){
+int age,loan;
+if(age>=21 &loan>=21000);
+printf("enter age:");
+scanf("%d" ,age=21);
+printf("enter loan:");
+scanf("%d" ,loan=21000);
+printf("congratulation you qualify for a loan");
+printf("unfortunately,we are unable to offer you a loan at this time");
+return 0;
+}
