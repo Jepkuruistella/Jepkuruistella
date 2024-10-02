61651a2b23f0bf3ba00d0498fddb4a03bf153c27
Jepkuruistella <jepkurui.22345@students.kyu.ac.ke>
2024-10-02 07:38:38 -0700

    Add files via upload

---
+++ library book (1).c
@@ -0,0 +1,28 @@
+//program to calculate fine overdue library 
+/*
+program that takes users input
+Author: stella  jepkurui
+Date:29/09/2024
+*/
+#include<stdio.h>
+int main () {
+printf("enter book ID",647);
+scanf( "enter ID%d",647);
+printf(" enter due date " ,01/9/2024);
+scanf("enter due date%d",01/9/2024);
+printf("enter return date",10/9/2024);
+scanf("enter return date%d",10/9/2024);
+//calculate date overdue=return date- due date;
+//determine the rate and fine amount;
+if(0<=7,20.0);
+if(8<=14,50.0);
+if(15<=22,100.0);
+//fine amount=fine rate* days overdue;
+printf("book ID:%d\n",647);
+printf("return date:%d\n",10/9/2024);
+printf("due date:%d\n",01/9/2024);
+printf("days overdue:%d\n",9);
+printf("fine rate:%.2f\n",50.0);
+printf("fine amount:%.2f\n",50.0);
+return 0;
+}
