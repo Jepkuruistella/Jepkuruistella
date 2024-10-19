bd6d818f3242f6716af179212d94bd31886daa07
Jepkuruistella <jepkurui.22345@students.kyu.ac.ke>
2024-10-19 05:11:27 -0700

    Add files via upload

---
+++ structure .c
@@ -0,0 +1,24 @@
+struct student{
+char name[30];
+float height;
+char reg_no[15];
+char email[20];
+int phone;
+}student1,student2;
+#include<stdio.h>
+#include<string.h>//strcpy
+int main(){
+//struct student student1,student2;
+//strcpy(student1.name,"stella");
+strcpy(student1.name,"stella");
+student1.height=5.7;
+strcpy(student1.reg_no,"ct101/g/22345/24");
+strcpy(student1.email,"stella@gmail.com");
+student1.phone=0716657360;
+printf("name:%s\n",student1.name);
+printf("height:%f\n",student1.height);
+printf("reg number:%s\n",student1.reg_no);
+printf("email:%s\n",student1.email);
+printf("phone number:%d\n", student1.phone);
+return 0;
+}
