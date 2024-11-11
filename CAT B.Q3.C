2c004172a74b121a94a1a7cd872babe1fb471f87
Jepkuruistella <jepkurui.22345@students.kyu.ac.ke>
2024-11-11 08:57:21 +0300

    Update CAT B.Q3.C

--- CAT B.Q3.C
+++ CAT B.Q3.C
@@ -1,33 +1,28 @@
 #include<stdio.h>
 int main(){
-float calculate_grosspay(int hours_worked, float hourly_wage){
-float grosspay;
-if(hours_worked <40){
-grosspay=hours_worked*hourly_wage;
-}
-else {
-grosspay=( 40*hourly_wage)+(hours_worked-40)*hourly_wage*1.5;
-}
-return grosspay;
-float calculate _taxes(float grosspay){
-float taxes;
-if (grosspay<600){
-taxes=grosspay*0.15;
-}
-else {
-taxes=(600*0.15)+(grosspay-600)*0.20;
-}
-return taxes;
-}
-printf("enter hours_worked in a week:);
-scanf("%d",&hours_worked);
-printf("enter hourly_wage:");
+float hours_worked,hourly_wage,gross_pay,tax,net_pay;
+//prompt the user to enter details
+printf("enter hours worked in a week:");
+scanf("%f",&hours_worked);
+printf("enter hourly wage;);
 scanf("%f",&hourly_wage);
-grosspay=hours_worked*hourly_wage;
-taxes=grosspay-taxes;
-printf("grosspay:$%.2f\n",grosspay);
-printf("taxes:$%.2f\n",taxes);
-printf("net pay:$%.2f\n",net pay);
+//calculate gross pay for overtime
+if(hours_worked>40){
+gross_pay=( 40*hourly_wage)+(hours_worked-40)*hourly_wage*1.5;
+}else{
+gross_pay=hours_workes*hourly_wage
 }
+//calculating tax
+if(gross_pay<=600){
+tax=gross_pay*0.15;
+}else{
+tax=(600*0.15)+((gross_pay-600)*0.20);
+}
+//calculating net pay
+net_pay=gross_pay-tax;
+//print results
+printf("grosspay:$%.2f\n",gross_pay);
+printf("tax:$%.2f\n",tax);
+printf("net pay:$%.2f\n",net_pay);
 return 0;
 }
