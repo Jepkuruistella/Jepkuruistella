3c95df83f60967b5d1d1e72f65abc0ab65c045a2
Jepkuruistella <jepkurui.22345@students.kyu.ac.ke>
2024-11-12 12:19:09 +0300

    Update 2D array (1).c

--- 2D array (1).c
+++ 2D array (1).c
@@ -6,12 +6,21 @@ date:16/10/2024
 */
 # include <stdio.h>
 int main(){
+int x,y;
 int arr[2][3]={ {1,2,3},{4,5,6} };
-for(int i=0;i<2;i++){
-for (int j=0;j<3;j++){
-printf("%d\n",arr[i][j]);
+for(int x=0;x<2;x++){
+for (int y=0;y<3;y++){
+printf("arr[%d][%d]=%d\n",x,y[x][y]);
 }
 }
-return 0;
+int s,t;
+for(s=0;s<x;s++){
+for(t=0;t<y;t++){
+printf("%d",arr[x][y]);
+}
+}
+printf("\n");
+
+return 0;"
 }
     
